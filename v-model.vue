cf7236d6fe7fcf3860d717b19051418341df6d7b
NanCylia <47407976+NanCylia@users.noreply.github.com>
2019-07-02 14:25:08 +0800

    Add files via upload

---
+++ v-model.vue
@@ -0,0 +1,531 @@
+<template>
+ <div id="app">
+   <div class="jd-page">
+     <div class="jd-page-title">
+       <ul class="jd-page-title-left">
+         <li><a href="#" >陕西</a></li>
+         <img src="../../../img/11111.png" class="jd-page-title-imgOne">
+       </ul>
+       <ul class="jd-page-title-right">
+         <li><a href="#">你好，请登录</a><a href="#" style="color: #dd0000;">免费注册</a></li>
+         <li class="v_line">|</li>
+         <li><a href="#">我的订单</a></li>
+         <li class="v_line">|</li>
+         <li class="titlehover"><a href="#">我的京东</a></li>
+         <li class="v_line">|</li>
+         <li><a href="#">京东会员</a></li>
+         <li class="v_line">|</li>
+         <li class="titlehover"><a href="#">企业采购</a></li>
+         <li class="v_line">|</li>
+         <li class="titlehover"><a href="#">客户服务</a></li>
+         <li class="v_line">|</li>
+         <li class="titlehover"><a href="#">网站导航</a></li>
+         <li class="v_line">|</li>
+         <li><a href="#">手机京东</a></li>
+       </ul>
+     </div>
+   </div>
+  <div class="jd-page-search-top">
+    <div class="jd-page-search">
+      <input type="text" class="jd-page-search-input">
+      <ul class="jd-page-search-bottom">
+        <li><a href="#" style="color: red">家电精选榜</a></li>
+        <li><a href="#" class="searchhover">空调钜惠</a></li>
+        <li><a href="#" class="searchhover">雅诗兰黛</a></li>
+        <li><a href="#" class="searchhover">夏季养殖</a></li>
+        <li><a href="#" class="searchhover">199减100</a></li>
+        <li><a href="#" class="searchhover">血压理疗</a></li>
+        <li><a href="#" class="searchhover">电脑数码</a></li>
+        <li><a href="#" class="searchhover">realme</a></li>
+        <li><a href="#" class="searchhover">冰淇淋</a></li>
+      </ul>
+      <img src="../../../img/134.png" class="jd-page-search-bottom-imgOne">
+      <img src="../../../img/154.png" class="jd-page-search-bottom-imgTwo">
+      <img src="../../../img/2019.png" class="jd-page-title-img">
+    </div>
+    <button class="jd-page-search-buttom">我的购物车</button>
+    <img src="../../../img/222.png" class="jd-page-search-bottom-imgThree">
+  </div>
+   <div class="jd-page-search-midder">
+     <ul class="jd-page-search-midder-ul">
+       <li><a href="#" class="midderhover">秒杀</a></li>
+       <li><a href="#" class="midderhover">优惠券</a></li>
+       <li><a href="#" class="midderhover">PLUS会员</a></li>
+       <li><a href="#" class="midderhover">闪购</a></li>
+       <li class="v_midder">|</li>
+       <li><a href="#" class="midderhover">拍卖</a></li>
+       <li><a href="#" class="midderhover">京东时尚</a></li>
+       <li><a href="#" class="midderhover">京东超市</a></li>
+       <li><a href="#" class="midderhover">京东生鲜</a></li>
+       <li class="v_midder">|</li>
+       <li><a href="#" class="midderhover">海囤全球</a></li>
+       <li><a href="#" class="midderhover">京东金融</a></li>
+     </ul>
+   </div>
+   <div class="jd-page-content">
+      <div class="jd-page-content-left-one">
+        <ul class="jd-left-bottom-ul">
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">家用电器</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">手机</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">运营商</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">数码</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">电脑</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">办公</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">家居</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">家具</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">家装</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">厨具</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">男装</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">女装</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">童装</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">内衣</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">美妆</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">个护清洁</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">宠物</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">女鞋</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">箱包</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">钟表</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">珠宝</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">男鞋</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">运动</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">户外</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">房产</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">汽车</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">汽车用品</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">母婴</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">玩具乐器</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">视频</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">酒类</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">生鲜</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">特产</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">艺术</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">礼品鲜花</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">农资绿植</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">医药保健</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">计生情趣</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">图书</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">文娱</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">电子书</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">机票</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">酒店</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">旅游</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">生活</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">理财</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">众筹</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">白条</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">保险</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">安装</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">清洗</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">维修</a>
+            <span class="jd-left-bottom-span">/</span>
+            <a href="#" class="jd-left-bottom-a">二手</a>
+          </li>
+          <li class="jd-left-bottom-li">
+            <a href="#" class="jd-left-bottom-a">工业品</a>
+          </li>
+        </ul>
+      </div>
+      <div class="jd-page-content-left">
+        <img src="https://img1.360buyimg.com/pop/s590x470_jfs/t1/65955/35/2504/102266/5d0c9e39E80946a76/9a3182437c66e841.jpg!q90!cc_590x470.webp" style="width: 470px;height: 440px">
+      </div>
+      <div class="jd-page-content-right-top">
+        <img src="https://img10.360buyimg.com/babel/s380x300_jfs/t1/55406/7/3270/105206/5d10305bEb135369a/d78e0507504ab8b9.jpg!q90!cc_190x150" style="width: 260px;height: 140px">
+      </div>
+      <div class="jd-page-content-right-mid">
+        <img src="https://img10.360buyimg.com/babel/s380x300_jfs/t1/45880/20/2832/83633/5d0ae172E113322a8/d8a0c2841288b243.jpg!q90!cc_190x150" style="width: 260px;height: 140px">
+      </div>
+      <div class="jd-page-content-right-bot">
+        <img src="https://img1.360buyimg.com/pop/s380x300_jfs/t1/43257/37/4186/51932/5cd155d5E714fa14b/8f5fefcfb9b9bac1.jpg!q90!cc_190x150" style="width: 260px;height: 140px">
+      </div>
+   </div>
+   <div class="jd-page-content-right-max">
+     <div class="jd-page-content-right-max-top">
+       <p>Hi~欢迎来到京东</p>
+       <p>登陆 注册</p>
+       <button class="jd-page-content-right-button-left">新人福利</button>
+       <button class="jd-page-content-right-button-right">PLUS会员</button>
+     </div>
+     <div class="jd-page-content-right-max-mid">
+       <span style="font-weight: bold ;float: left;font-size: 15px;" >京东快报</span>
+       <span style="float: right">更多></span><br>
+     </div>
+     <div class="jd-page-content-right-max-mid-two">
+       <div class="jd-page-content-right-max-mid-two-button">
+         <button style="color: white;background-color: #e47f7f; border: 0px ;width: 40px;height: 20px;margin: 2px 0 2px 0 ">热议</button>
+         <button style="color: white;background-color: #e47f7f; border: 0px ;width: 40px;height: 20px;margin: 2px 0 2px 0 ">热门</button>
+         <button style="color: white;background-color: #e47f7f; border: 0px ;width: 40px;height: 20px;margin: 2px 0 2px 0 ">最新</button>
+         <button style="color: white;background-color: #e47f7f; border: 0px ;width: 40px;height: 20px;margin: 2px 0 2px 0 ">热评</button>
+       </div>
+     </div>
+   </div>
+ </div>
+</template>
+<script>
+</script>
+<style>
+  #app{
+    margin: 0px;
+    padding: 0px;
+  }
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  body{
+    background-color: #f0f3ef;
+  }
+  .jd-page{
+    background-color: #e3e4e5;
+  }
+.jd-page-title{
+  width: 1190px;
+  height: 30px;
+  margin: 0 auto;
+  line-height: 30px;
+}
+.jd-page-title-imgOne{
+  width: 20px;
+  height: 25px;
+  position: relative;
+  top: -30px;
+  left: -23px;
+}
+  .jd-page-title-left{
+    margin-left: 200px;
+    float: left;
+  }
+  .jd-page-title-left > li{
+    border-left: 1px solid #e3e4e5;
+    border-right: 1px solid #e3e4e5;
+    list-style: none;
+    font-size: 12px;
+    color: #a3a3a3;
+    padding: 0 7px;
+  }
+  .jd-page-title-left > li > i {
+    color: red;
+    font-size: 12px;
+    margin-left: 5px;
+  }
+  .jd-page-title-left > li > a {
+    color: #a3a3a3;
+    text-decoration: none;
+  }
+  .jd-page-title-left > li:hover{
+    background-color: white;
+    border-left: 1px solid silver;
+   border-right: 1px solid silver;
+  }
+  .jd-page-title-right{
+    float: right;
+  }
+  .jd-page-title-right > li{
+    float: left;
+    margin-right: 0px;
+    list-style: none;
+    font-size: 12px;
+    color: #a3a3a3;
+  }
+  .jd-page-title-right > li > a{
+    color: #a3a3a3;
+    margin: 0 7px;
+    text-decoration: none;
+  }
+  .jd-page-title-right > li > i{
+    margin-left: 10px;
+    color: silver;
+  }
+  .titlehover{
+    border-right: 1px solid #e3e4e5;
+    border-left: 1px solid #e3e4e5;
+    margin-left: 7px;
+    margin-right: 7px;
+  }
+  .titlehover:hover{
+    background-color: #fff;
+    border-left: 1px solid silver;
+    border-right: 1px solid silver;
+  }
+  .jd-page-title-right > li > a:hover{
+    color: #dd0000;
+  }
+  .v_line{
+    height: 10px;
+  }
+  .jd-page-title-img{
+    width: 160px;
+    height: 140px;
+    float: left;
+    position: relative;
+    top: -144px;
+    left: -264px;
+  }
+  .jd-page-search-top{
+    width: 1290px;
+    height: 100px;
+    /*background-color: green;*/
+  }
+  .jd-page-search{
+    width: 625px;
+    height: 85px;
+    margin:  38px auto;
+  }
+  .jd-page-search-input{
+    width: 625px;
+    height: 35px;
+    border: white;
+  }
+  .jd-page-search-bottom > li {
+    list-style: none;
+    font-size: 12px;
+    float: left;
+    color: #a3a3a3;
+    margin-right: 0;
+    margin-top: 10px;
+  }
+  .jd-page-search-bottom > li > a{
+    color: #a3a3a3;
+    margin: 0 7px;
+    text-decoration: none;
+  }
+ .jd-page-search-bottom > li > a:hover{
+   color: red;
+ }
+  .jd-page-search-bottom-imgOne{
+    width: 21px;
+    height: 25px;
+    position: relative;
+    top: -40px;
+    left: -8px;
+  }
+  .jd-page-search-bottom-imgTwo{
+    width: 53px;
+    height: 35px;
+    position: relative;
+    top: -35px;
+  }
+  .jd-page-search-buttom{
+    background-color: #fff;
+    border: 1px solid #e3e4e5;
+    width: 180px;
+    height: 35px;
+    float: right;
+    position: relative;
+    top: -123px;
+    left: -58px;
+    color: #ff0000;
+    font-size: 15px;
+  }
+  .jd-page-search-bottom-imgThree{
+    width: 25px;
+    height: 25px;
+    float: right;
+    position: relative;
+    top: -119px;
+    left: -12px;
+  }
+  .jd-page-search-midder{
+    width: 1290px;
+    height: 55px;
+    /*background-color: orangered;*/
+  }
+  .jd-page-search-midder-ul{
+    float: left;
+    position: relative;
+    left: 239px;
+    top:-120px;
+  }
+  .jd-page-search-midder-ul > li {
+    list-style: none;
+    font-size: 15px;
+    float: left;
+    color: black;
+  }
+  .jd-page-search-midder-ul > li > a{
+    margin: 0 15px;
+    text-decoration: none;
+    color: black;
+  }
+  .jd-page-search-midder-ul > li > a:hover{
+    color: red;
+  }
+  .jd-page-content{
+    width:750px ;
+    height: 452px;
+    /*border: 1px solid black;*/
+    margin: auto;
+    position: relative;
+    top: -90px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
+  .jd-page-content-left{
+    width: 480px;
+    height: 450px;
+    /*border: 1px solid red;*/
+  }
+  .jd-page-content-right-top{
+    width: 270px;
+    height: 150px;
+    /*border: 1px solid blue;*/
+  }
+  .jd-page-content-right-mid{
+    width: 270px;
+    height: 150px;
+    /*border: 1px solid green;*/
+  }
+  .jd-page-content-right-bot{
+    width: 270px;
+    height: 150px;
+    /*border: 1px solid yellow;*/
+  }
+  .jd-page-content-left-one{
+    width: 160px;
+    height: 450px;
+    /*border: 1px solid black ;*/
+    margin-left: -130px;
+    position: relative;
+    top: -75px;
+    left: -58px;
+  }
+ .jd-page-content-title > li{
+   list-style: none;
+   font-size: 12px;
+   line-height: 25px;
+ }
+  .jd-page-content-title > li > a:hover{
+    color: red;
+  }
+  .jd-page-content-right-max{
+    width: 200px;
+    height:440px ;
+   /*border: 1px solid red;*/
+    position: relative;
+    top: -542px;
+    float: right;
+  }
+  .jd-page-content-right-max-top{
+    width: 200px;
+    height: 140px;
+    line-height: 40px;
+    /*font-size: 15px;*/
+    color: #a3a3a3;
+  }
+  .jd-page-content-right-button-left{
+    width: 60px;
+    height: 30px;
+    color: red;
+    background-color: #fff;
+    border-radius: 13px;
+    border: 0;
+  }
+  .jd-page-content-right-button-right{
+    width: 60px;
+    height: 30px;
+    color: #ebf34a;
+    background-color: black;
+    border-radius: 13px;
+    border: 0;
+  }
+  .jd-page-content-right-max-mid{
+    width: 200px;
+    height: 30px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .jd-page-content-right-max-mid-two{
+    width: 200px;
+    height: 100px;
+    padding-left: 10px;
+    padding-right: 15px;
+  }
+  .jd-page-content-right-max-mid-two-button{
+    width: 40px;
+    display: flex;
+    flex-direction: column;
+  }
+  .jd-left-bottom-ul > li{
+    list-style: none;
+    font-size: 14px;
+    color: #636363;
+    margin-right: 0px;
+  }
+  .jd-left-bottom-ul{
+    padding: 6px 0;
+  }
+  .jd-left-bottom-a{
+    color: #636363;
+  }
+  .jd-left-bottom-a:hover{
+    color: red;
+  }
+</style>
